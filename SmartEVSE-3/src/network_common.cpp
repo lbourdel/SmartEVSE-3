5ad94458c68a1f587b35700c46a77401aa1e255a
Laurent BOURDEL <laurent.bourdel@gmail.com>
2026-05-12 21:38:34 +0200

    update

--- SmartEVSE-3/src/network_common.cpp
+++ SmartEVSE-3/src/network_common.cpp
@@ -988,7 +988,8 @@ static void fn_http_server(struct mg_connection *c, int ev, void *ev_data) {
 #endif
                 _LOG_A("Connecting to wifi network.\n");
                 WiFi.mode(WIFI_STA);                // Set Station Mode
-const uint8_t bssid_u8[6] = {0x8C,0x97,0xEA,0xA9,0x13,0x40}; // Repeteur Free
+// 8C:97:EA:A9:13:40
+                const uint8_t bssid_u8[] = {0x8C,0x97,0xEA,0xA9,0x13,0x40}; // Repeteur Free
                 WiFi.begin(ssid, password, 0, bssid_u8);   // Configure Wifi with credentials
                 // WiFi.begin(ssid, password);   // Configure Wifi with credentials
                 WIFImode = 1;                                                           // we are already connected so don't call handleWIFImode
@@ -1376,7 +1377,7 @@ void onWifiEvent(WiFiEvent_t event, WiFiEventInfo_t info) {
             uint8_t password[65] = { 0 };
             memcpy(ssid, info.sc_got_ssid_pswd.ssid, sizeof(info.sc_got_ssid_pswd.ssid));
             memcpy(password, info.sc_got_ssid_pswd.password, sizeof(info.sc_got_ssid_pswd.password));
-const uint8_t bssid_u8[6] = {0x8C,0x97,0xEA,0xA9,0x13,0x40}; // Repeteur Free
+const uint8_t bssid_u8[] = {0x8C,0x97,0xEA,0xA9,0x13,0x40}; // Repeteur Free
             WiFi.begin((char*)ssid, (char *)password, 0, bssid_u8);
             // WiFi.begin((char*)ssid, (char *)password);
         }
